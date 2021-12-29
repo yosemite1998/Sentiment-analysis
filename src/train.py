ea108ede848f0f751344f233300115285587297d
yosemite1998 <33573084+yosemite1998@users.noreply.github.com>
2021-12-29 20:19:08 +0800

    submit train.py

--- src/train.py
+++ src/train.py
@@ -60,8 +60,8 @@ def run():
     param_optimizer = list(model.named_parameters())
     no_decay = ["bias","LayerNorm.bias","LayerNorm.weight"]
     optimizer_parameters = [
-        {'param': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)]},
-        {'param': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)]},
+        {'param': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],'weight_decay':0.001},
+        {'param': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],'weight_decay':0.0}
     ]
 
     #num_train_steps = int(len(df_train)/args.train_batch_size * args.epochs)
