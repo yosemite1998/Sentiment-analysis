9391807e75a22e272f22ed99e2794fc3cffacffc
yosemite1998 <33573084+yosemite1998@users.noreply.github.com>
2021-12-29 20:08:43 +0800

    submit

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ class args:
     train_batch_size = 8
     valid_batch_size = 4
     epochs = 10
-    bert_path = "../input/huggingface-bert/bert-base-cased"
-    model_path = "pytoch_model.bin"
-    training_file = "../input/imdb-dataset-of-50k-movie-reviews/IMDB Dataset.csv"
+    bert_path = "/kaggle/input/huggingface-bert/bert-base-cased"
+    model_path = "pytorch_model.bin"
+    training_file = "/kaggle/input/imdb-dataset-of-50k-movie-reviews/IMDB Dataset.csv"
     tokenizer = transformers.BertTokenizer.from_pretrained(
         bert_path,
         do_lower_case=True
