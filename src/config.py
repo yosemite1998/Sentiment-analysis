48198ad36c4f7757fe9899e3e4438a58445faf12
yosemite1998 <33573084+yosemite1998@users.noreply.github.com>
2021-12-29 19:24:32 +0800

    submit config.py

--- src/config.py
+++ src/config.py
@@ -5,9 +5,9 @@ class args:
     train_batch_size = 8
     valid_batch_size = 4
     epochs = 10
-    bert_path = "../input/bert-base-uncased/"
+    bert_path = "bert-base-uncased"
     model_path = "pytoch_model.bin"
-    training_file = "../input/imdb.csv"
+    training_file = "../input/imdb-dataset-of-50k-movie-reviews/IMDB Dataset.csv"
     tokenizer = transformers.BertTokenizer.from_pretrained(
         bert_path,
         do_lower_case=True
