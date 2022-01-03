0fc3d6d66c4c8cb3c52049685f4154a4db747328
yosemite1998 <33573084+yosemite1998@users.noreply.github.com>
2022-01-03 15:30:34 +0800

    dfx

--- src/search_maxlen_plot.py
+++ src/search_maxlen_plot.py
@@ -1,9 +1,14 @@
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import config
+
 max_len=[]
+
+dfx = pd.read_csv(config.args.training_file,nrows=2000).fillna("none")
 for text in dfx.review.values:
-    token = args.tokenizer.encode(text,max_length=512,truncation=True)
+    token = config.args.tokenizer.encode(text,max_length=512,truncation=True)
     max_len.append(len(token))
 
 sns.displot(max_len)
